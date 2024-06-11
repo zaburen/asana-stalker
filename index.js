0c32259a93e3c8b73efaae989b3dd9d3731e7dcf
zack <zcdorman@gmail.com>
2024-06-11 20:50:49 +0900

    forgot run function

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 const UtilUrl = require("./util-url");
 const CustomAsana = require("./asana");
 const CustomGithub = require("./github");
+
+function run() {
+    const eventBody = CustomGithub.getGithubEventsBody();
+    const eventUrl = CustomGithub.getEventUrl();
+    const hrefText = CustomGithub.getEventHrefText();
+    const linkList = UtilUrl.getAnyAsanaUrls(eventBody);
+    linkList.forEach((obj) => {
+        CustomAsana.getTaskNotesAndUpdateThemIfNeeded(obj.gid, eventUrl, hrefText);
+    });
+}
+
+run();
